593a4bce2e7f1765470bcaf5bc67bffe76734a19
Adam <adamos2375@gmail.com>
2019-11-13 21:25:02 +0100

    Update sh_um_uniform.lua

--- gamemodes/ixhl2rp/schema/items/outfit/sh_um_uniform.lua
+++ gamemodes/ixhl2rp/schema/items/outfit/sh_um_uniform.lua
@@ -3,5 +3,5 @@ ITEM.name = "Uniform członka UM"
 ITEM.description = "Uniform noszony przez członków UM"
 ITEM.category = "Clothing"
 ITEM.replacements = "models/betacz/group03m/male_09.mdl"
-
+ITEM.model = "models/props_junk/PlasticCrate01a.mdl"
 
