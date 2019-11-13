0e34df37ea35b9f09af9df7033908f23a3619876
Adam <adamos2375@gmail.com>
2019-11-13 22:33:49 +0100

    Update sh_unionmedical.lua

--- gamemodes/ixhl2rp/schema/factions/sh_unionmedical.lua
+++ gamemodes/ixhl2rp/schema/factions/sh_unionmedical.lua
@@ -1,5 +1,5 @@
 FACTION.name = "Union Medical"
-FACTION.description = "Uformowane spośród przedwojennych lekarzy, studentów i studentek medycyny i pielęgniarstwa, a także wszystkich wystarczająco sprytnych, by obandażować kogoś rannego przez maszynę podczas cyklu pracy.\n Nadrzędnym celem UM jest kontrola stanu zdrowia mieszkańców, a także kontrola warunków sanitarnych w Mieście."
+FACTION.description = "Uformowane spośród przedwojennych lekarzy, studentów i studentek medycyny i pielęgniarstwa, a także wszystkich wystarczająco sprytnych, by obandażować kogoś rannego przez maszynę podczas cyklu pracy.\n Nadrzędnym celem UM jest kontrola stanu zdrowia mieszkańców, a także pilnowanie warunków sanitarnych w Mieście 17."
 FACTION.color = Color(255, 121, 121)
 FACTION.pay = 5
 FACTION.isDefault = false
