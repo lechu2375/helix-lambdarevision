56ea8777a78797edee7e8ac1edc02b1137aa3b6b
Adam <adamos2375@gmail.com>
2019-11-13 21:31:38 +0100

    opis um

--- gamemodes/ixhl2rp/schema/factions/sh_unionmedical.lua
+++ gamemodes/ixhl2rp/schema/factions/sh_unionmedical.lua
@@ -1,5 +1,5 @@
 FACTION.name = "Union Medical"
-FACTION.description = "A metropolice unit working as Civil Protection."
+FACTION.description = "Uformowane spośród przedwojennych lekarzy, studentów i studentek medycyny i pielęgniarstwa, a także wszystkich wystarczająco sprytnych, by obandażować kogoś rannego przez maszynę podczas cyklu pracy.\n Nadrzędnym celem UM jest kontrola stanu zdrowia mieszkańców, a także kontrola warunków sanitarnych w Mieście."
 FACTION.color = Color(255, 121, 121)
 FACTION.pay = 5
 FACTION.isDefault = false
