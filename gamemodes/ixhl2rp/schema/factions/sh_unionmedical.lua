97ff9de2d52e034755e4dd7db269099428f1e992
Adam <adamos2375@gmail.com>
2019-11-13 21:12:44 +0100

    Frakcja UM + Item ubieralny uniformu

---
+++ gamemodes/ixhl2rp/schema/factions/sh_unionmedical.lua
@@ -0,0 +1,12 @@
+FACTION.name = "Union Medical"
+FACTION.description = "A metropolice unit working as Civil Protection."
+FACTION.color = Color(255, 121, 121)
+FACTION.pay = 5
+FACTION.isDefault = false
+FACTION.isGloballyRecognized = false
+
+
+
+
+
+FACTION_UM = FACTION.index
---
+++ gamemodes/ixhl2rp/schema/items/outfit/sh_um_uniform.lua
@@ -0,0 +1,7 @@
+
+ITEM.name = "Uniform członka UM"
+ITEM.description = "Uniform noszony przez członków UM"
+ITEM.category = "Clothing"
+ITEM.replacements = "models/betacz/group03m/male_09.mdl"
+
+
