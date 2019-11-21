fa1a7c73b40429166bf20460fae844db3f1a9a10
Adam <adamos2375@gmail.com>
2019-11-21 17:33:23 +0100

    Końcowy(mam nadzieję) commit opasek + dodanie itemów opasek

--- gamemodes/ixhl2rp/plugins/bands/cl_hooks.lua
+++ gamemodes/ixhl2rp/plugins/bands/cl_hooks.lua
@@ -1,8 +1,19 @@
+local bands = { --taka tablica, żeby nie zapierdalać po tabelach itemku
+	["brown"] = {Color(102, 51, 51),"Brązowa opaska lojalisty"},
+	["red"] = {Color(192, 57, 43),"Czerwona opaska lojalisty"},
+	["green"] = {Color(39, 174, 96),"Zielona opaska lojalisty"},
+	["blue"] = {Color(41, 128, 185),"Niebieska opaska lojalisty"},
+	["white"] = {Color(221, 221, 221),"Biała opaska lojalisty"},
+	["gold"] = {Color(241, 196, 15),"Złota opaska lojalisty"},
+	["violet"] = {Color(142, 68, 173),"Fioletowa opaska lojalisty"}
+}
+
 function PLUGIN:PopulateCharacterInfo(client, character, tooltip)
-	if client then
+	local band = client:GetNW2String("band") --string z typem opaski, czyli można rzec, że kolorem
+	if band then --jako, że po zdjęciu banda NWString jest nilem to można zajebać takiego checka
 		local panel = tooltip:AddRowAfter("name", "band")
-		panel:SetBackgroundColor(Color(255, 127, 80))
-		panel:SetText("Opaska CWU")
+		panel:SetBackgroundColor(bands.band[1]) 
+		panel:SetText(bands.band[2])
 		panel:SizeToContents()
     end
-end
+end	
