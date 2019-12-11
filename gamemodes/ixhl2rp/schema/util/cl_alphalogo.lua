bdd42336d2c92feb107e48111dfc9db065b486ca
Adam <adamos2375@gmail.com>
2019-12-11 11:31:47 +0100

    Update cl_alphalogo.lua

--- gamemodes/ixhl2rp/schema/util/cl_alphalogo.lua
+++ gamemodes/ixhl2rp/schema/util/cl_alphalogo.lua
@@ -1,6 +1,8 @@
-local text = ("Lambda Revision - Alpha "..Schema.version)
-local x,y = surface.GetTextSize(text)
-x=x+40
+do
+    local text = ("Lambda Revision - Alpha "..Schema.version)
+    local x,y = surface.GetTextSize(text)
+    x=x+40
+end
 function Schema:HUDPaint()
     draw.SimpleText( text, "ixCombineViewData", ScrW()-x,0, color_white)
     draw.SimpleText( "Wszystko może ulec zmianie", "ixCombineViewData", ScrW()-x,y, color_white)
