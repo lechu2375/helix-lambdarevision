2064da08a5f5bbc41e1eca6242bee21f100469f1
Adam <adamos2375@gmail.com>
2019-12-11 11:45:31 +0100

    Pozycjonowanie tekstu w rogu

--- gamemodes/ixhl2rp/schema/util/cl_alphalogo.lua
+++ gamemodes/ixhl2rp/schema/util/cl_alphalogo.lua
@@ -1,8 +1,6 @@
-do
     local text = ("Lambda Revision - Alpha "..Schema.version)
+    surface.SetFont("ixCombineViewData")
     local x,y = surface.GetTextSize(text)
-    x=x+40
-end
 function Schema:HUDPaint()
     draw.SimpleText( text, "ixCombineViewData", ScrW()-x,0, color_white)
     draw.SimpleText( "Wszystko może ulec zmianie", "ixCombineViewData", ScrW()-x,y, color_white)
