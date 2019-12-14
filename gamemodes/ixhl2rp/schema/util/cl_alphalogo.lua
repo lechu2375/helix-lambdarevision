f9f18659494e3e73a23bddbdaf33f89f6228a8fa
Adam <adamos2375@gmail.com>
2019-12-14 19:12:30 +0100

    Wyświetlanie tekstu w rogu rozpoczyna się po załadowaniu się czcionek.

--- gamemodes/ixhl2rp/schema/util/cl_alphalogo.lua
+++ gamemodes/ixhl2rp/schema/util/cl_alphalogo.lua
@@ -1,8 +1,12 @@
+
+function Schema:LoadFonts()
     local text = ("Lambda Revision - Alpha "..Schema.version)
     surface.SetFont("ixCombineViewData")
     local x,y = surface.GetTextSize(text)
+end
 function Schema:HUDPaint()
     draw.SimpleText( text, "ixCombineViewData", ScrW()-x,0, color_white)
     draw.SimpleText( "Wszystko może ulec zmianie", "ixCombineViewData", ScrW()-x,y, color_white)
 end
+
 
