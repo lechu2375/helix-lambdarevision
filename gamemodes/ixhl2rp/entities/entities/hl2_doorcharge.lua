5e633f42d4edff34cb20d827c8d227801160cf65
Adam <adamos2375@gmail.com>
2019-12-18 19:49:20 +0100

    Pikanie zależne od ilości sekund pozostałych do wybuchu ładunku

--- gamemodes/ixhl2rp/entities/entities/hl2_doorcharge.lua
+++ gamemodes/ixhl2rp/entities/entities/hl2_doorcharge.lua
@@ -9,7 +9,9 @@ ENT.AdminOnly = true
 ENT.PhysgunDisable = true
 ENT.bNoPersist = true
 
-
+function ENT:SetupDataTables()
+	self:NetworkVar("Bool", 0, "Armed")
+end
 
 if (SERVER) then
 	function ENT:GetLockPosition(door, normal)
@@ -21,11 +23,11 @@ if (SERVER) then
 			position = door:GetBonePosition(index)
 		end
 
-		position = position + normal:Forward() * 7.2 + normal:Up() * 10 + normal:Right() * 2
+		position = position + normal:Forward() * 3.2 + normal:Up() * 10 + normal:Right() * 2
 
-		normal:RotateAroundAxis(normal:Up(), 90)
-		normal:RotateAroundAxis(normal:Forward(), 180)
-		normal:RotateAroundAxis(normal:Right(), 180)
+		normal:RotateAroundAxis(normal:Up(), 180)
+		normal:RotateAroundAxis(normal:Forward(), 90)
+		normal:RotateAroundAxis(normal:Right(), 90)
 
 		return position, normal
 	end
@@ -39,12 +41,12 @@ if (SERVER) then
 
 		self.door = door
 		self.door:DeleteOnRemove(self)
-		door.ixLock = self
+
 
 		if (IsValid(doorPartner)) then
 			self.doorPartner = doorPartner
 			self.doorPartner:DeleteOnRemove(self)
-			doorPartner.ixLock = self
+			
 		end
 
 		self:SetPos(position)
@@ -61,7 +63,6 @@ if (SERVER) then
 
 		local normal = client:GetEyeTrace().HitNormal:Angle()
 		local position, angles = self:GetLockPosition(door, normal)
-		self.creator = client
 		local entity = ents.Create("hl2_doorcharge")
 		entity:SetPos(trace.HitPos)
 		entity:Spawn()
@@ -72,7 +73,7 @@ if (SERVER) then
 	end
 
 	function ENT:Initialize()
-		self:SetModel("models/props_combine/combine_lock01.mdl")
+		self:SetModel("models/warz/items/c4.mdl")
 		self:SetSolid(SOLID_VPHYSICS)
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
@@ -87,15 +88,25 @@ if (SERVER) then
 		end
 	end
 
-
-
 	function ENT:Toggle(client)
 		local id = self:GetCreationID()
-			if not timer.Exists("doorcharge"..id) then
+		if not timer.Exists("doorcharge"..id) then
+			self.toggle = true
+			local ent = self
 			timer.Create("doorcharge"..id, 5,1, function()
-				util.BlastDamage(self.creator,self,self:GetPos(), 100, 100)
-				self:Remove()
+				local explode = ents.Create( "env_explosion" ) 
+				local explo = ents.Create( "env_explosion" )
+				explo:SetPos( ent:GetPos() )
+				explo:Spawn()
+				explo:Fire( "Explode" )
+				explo:SetKeyValue( "IMagnitude", 20 )			
+				ent.door:Fire("Unlock")
+				ent:Remove()
 			end)
+			self.nextbeep = CurTime()+1
+			self:SetArmed(true)
+			self:EmitSound("hl1/fvox/beep.wav")
+			
 		end
 	end
 
@@ -103,16 +114,42 @@ if (SERVER) then
 		self:Toggle(client)
 	end
 else
+ 
+
 	local glowMaterial = ix.util.GetMaterial("sprites/glow04_noz")
 	local color_red = Color(255, 50, 50, 255)
-
+	local color_green = Color(68, 189, 50)
 	function ENT:Draw()
 		self:DrawModel()
 
-		local color = color_red
-		local position = self:GetPos() + self:GetUp() * -8.7 + self:GetForward() * -3.85 + self:GetRight() * -6
+		if self:GetArmed() then 
+			color = color_red
+		else
+			color = color_green
+		end
+		local position = self:GetPos() + self:GetUp() * 2
 
 		render.SetMaterial(glowMaterial)
 		render.DrawSprite(position, 10, 10, color)
 	end
+	function ENT:Think()
+		if self:GetArmed() then
+			if not self.nextbeep then 
+				self.beeptime = 1
+				self.nextbeep = CurTime() + self.beeptime
+				--self.beeps = 0
+			end
+			if self.nextbeep<CurTime() then --and self.beeps<=2
+				print("beepnext"..self.beeptime)
+				self:EmitSound("hl1/fvox/beep.wav")
+				self.nextbeep = CurTime()+ self.beeptime
+				--self.beeps = self.beeps +1
+				if self.beeptime<0.3 then return end 
+				self.beeptime = self.beeptime*.6
+			end
+		end
+	end
+
 end
+
+
