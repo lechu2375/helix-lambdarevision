8df05f7f04b642bbcaf7aa9c607a114cca27ed4e
Adam <adamos2375@gmail.com>
2019-12-18 20:49:57 +0100

    nowe ubranko

---
+++ gamemodes/ixhl2rp/schema/items/outfit/sh_rodrik_outfit.lua
@@ -0,0 +1,10 @@
+
+ITEM.name = "Szmaty Rodrika"
+ITEM.description = "Ubranie dające jako taką ochronę przed otarciami"
+ITEM.category = "Clothing"
+
+
+ITEM.replacements = "models/tactical_rebel.mdl"
+
+
+
