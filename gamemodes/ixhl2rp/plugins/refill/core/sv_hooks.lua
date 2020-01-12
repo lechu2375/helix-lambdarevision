f05fc2c9f0f3fcfe49857b495b1c0392d2fd54b6
Adam <adamos2375@gmail.com>
2020-01-12 20:13:56 +0100

    huj

--- gamemodes/ixhl2rp/plugins/refill/core/sv_hooks.lua
+++ gamemodes/ixhl2rp/plugins/refill/core/sv_hooks.lua
@@ -1,7 +1,28 @@
+print("sh")
 function PLUGIN:PlayerUse(ply,ent)
-    if ent:GetClass()=="ix_vendor" then
+    local class = ent:GetClass()
+    if class == "ix_vendor" then
         local items = ent.items
         local toResupply = 0
+        for k,v in pairs(items) do --ENT:SetStock(uniqueID, value)
+            if v[VENDOR_MAXSTOCK] and v[VENDOR_MAXSTOCK]>0 then
+                print("===")
+                print(k)                
+                PrintTable(v)
+                print("Stock:"..v[VENDOR_STOCK])
+                print("\n MaxStock:"..v[VENDOR_MAXSTOCK])
+                toResupply = toResupply + (v[VENDOR_MAXSTOCK] - v[VENDOR_STOCK])
+            end
+        end
+        PrintMessage(HUD_PRINTTALK,toResupply)
+    elseif class == "ix_vendingmachine" then --ENT:ResetStock(id)
+        local items = ent.Items
+        local toResupply = #ent.Items*ent.MaxStock
+        for k,v in pairs(items) do
+            print(k.."curr stock:"..ent:GetStock(k))
+            toResupply = toResupply-ent:GetStock(k)
+        end
+        print("toresupply:"..toResupply)
     end
 
 end
--- gamemodes/ixhl2rp/plugins/refill/sh_plugin.lua
+++ gamemodes/ixhl2rp/plugins/refill/sh_plugin.lua
@@ -1,4 +1,5 @@
 PLUGIN.name = "Refill'ed"
 PLUGIN.author = "Lechu2375"
 PLUGIN.desc = "Allows players to refill stock of the vendor, vending machines and event ration distributors."
-ix.util.IncludeDir("core")
+ix.util.Include("core/sv_core.lua")
+ix.util.Include("core/sv_hooks.lua")
